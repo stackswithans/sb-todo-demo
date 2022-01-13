8dc57a41c8c194015360cb0ff8cdc59d32839c27
stackswithans <stexor12@gmail.com>
2022-01-13 23:02:27 +0100

    Return Id

--- src/main/java/com/todo/demo/models/Todo.java
+++ src/main/java/com/todo/demo/models/Todo.java
@@ -1,18 +1,27 @@
 package com.todo.demo.models;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("todos")
 public class Todo {
+    @Id
+    private ObjectId id;
     private String desc;
     private String place;
     private String dueDate;
     private boolean done = false;
 
-    public Todo(String desc, String place, String dueDate) {
+    public Todo(String desc, String place, String dueDate, boolean done) {
         this.desc = desc;
         this.place = place;
         this.dueDate = dueDate;
+        this.done = done;
+    }
+
+    public ObjectId getId() {
+        return id;
     }
 
     public String getDesc() {
